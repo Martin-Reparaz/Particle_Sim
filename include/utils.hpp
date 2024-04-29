97959522be76051acdef14c6c4f10b244e49e91d
Martín J. Repáraz <105919205+Martin-Reparaz@users.noreply.github.com>
2024-04-29 08:52:36 +0200

    Variable missnamed fix.

--- include/utils.hpp
+++ include/utils.hpp
@@ -23,7 +23,7 @@ void limit_velocity(Point &point);
 
 bool check_collision(const Point &point, const Point &point2);
 
-void handle_collision(Point &point, Point &point2);
+void handle_collision(Point &point1, Point &point2);
 
 void read_params(int &SCREEN_WIDTH, int &SCREEN_HEIGHT, int &POINT_SIZE, int &NUM_POINTS, float &MAX_VELOCITY,
                  float &BASE_GRAVITY_CONSTANT, float &MAX_GRAVITY_RANGE, float &MOUSE_INFLUENCE_RADIUS,
